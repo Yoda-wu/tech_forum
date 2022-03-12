c3885d247752d2f85249495b7fd47e8b2a70738a
Marchons <d1286360646@outlook.com>
2022-03-12 19:54:04 +0800

    添加mybaitis-plus配置

--- project-app/src/main/java/com/uml/projectapp/controller/TestController.java
+++ project-app/src/main/java/com/uml/projectapp/controller/TestController.java
@@ -1,6 +1,6 @@
 package com.uml.projectapp.controller;
 
-import com.uml.service.TestService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class TestController {
-    @Autowired
-    private TestService testService;
 
-    @GetMapping("/test")
-    public String test(){
-        return testService.test();
-    }
+
     @GetMapping("/test2")
     public String test2(){
         return "testService.test()";
---
+++ project-common/src/main/java/com/uml/common/CommonMain.java
@@ -0,0 +1,11 @@
+package com.uml.common;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wuyuda
+ * @date 2022-03-06 13:57
+ */
+@SpringBootApplication
+public class CommonMain {
+}
