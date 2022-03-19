7a3af4ccaf9f161aae8e276f37241e78dad31161
Marchons <d1286360646@outlook.com>
2022-03-19 16:19:38 +0800

    mybatisplus配置类

---
+++ project-app/src/main/java/com/uml/projectapp/config/MybatisPlusConfig.java
@@ -0,0 +1,27 @@
+package com.uml.projectapp.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author wuyuda
+ * @date 2022-03-19 16:15
+ */
+@Configuration
+@EnableTransactionManagement
+public class MybatisPlusConfig {
+
+    /**
+     *   注册乐观锁
+     */
+    @Bean
+    public MybatisPlusInterceptor paginationInnerInterceptor(){
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        return mybatisPlusInterceptor;
+    }
+}
